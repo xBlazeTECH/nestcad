403723d2fe6d21228030c7072ed5d242a4d4d61c
xBlazeTECH <xBlazeTECH@users.noreply.github.com>
2020-05-20 11:25:54 -0400

    Add App Service

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,13 @@
 import { Controller, Get, Render } from '@nestjs/common';
+import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
+  constructor(private readonly appService: AppService) {}
+
   @Get()
   @Render('index')
-  root() {
-    return { message: 'Hello world!' };
-  }
+  index() {
+    return { message: this.appService.getIndex() };
+  }  
 }
